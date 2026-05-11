7d99b69e15c079fe53cf6940cbc5169a0279ccae
JoseLeac <jos.duranc@duocuc.cl>
2026-05-11 03:20:10 -0400

    Actualizacion

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2/promise");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+//prueba
 const {
   DB_HOST = "10.0.152.3", // acá colocar la IP Privada EC2 DB
   DB_USER = "root",
