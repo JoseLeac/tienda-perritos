65cce3edcb4b94ada5119a50a1fefbc849bf1b7f
jos.duranc@duouc.cl <jos.duranc@duocuc.cl>
2026-05-12 22:40:00 -0400

    Prueba docker

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2/promise");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-//prueba 1
+//prueba 2
 const {
   DB_HOST = "10.0.152.3", // acá colocar la IP Privada EC2 DB
   DB_USER = "root",
