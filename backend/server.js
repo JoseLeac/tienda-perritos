b288189ac6e54283b713a3ed9031bf8babfd8a4e
jos.duranc@duouc.cl <jos.duranc@duocuc.cl>
2026-05-12 23:25:24 -0400

    Actualizacion

--- backend/server.js
+++ backend/server.js
@@ -153,7 +153,7 @@ app.get("/api/health", (req, res) => {
   });
 });
 
-// Iniciar servidor 2.0 ahora
+// Iniciar servidor 2.0
 app.listen(PORT, async () => {
   console.log(`Servidor backend escuchando en puerto ${PORT}`);
   await initDb();
